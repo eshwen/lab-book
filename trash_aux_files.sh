800deefbb8ab6adf82331743e5b2ea3894b85168
eshwen <eshwen.bhal@bristol.ac.uk>
2020-04-23 12:48:40 +0100

    Cleaning lots of stuff up. Using texttt instead of verb. Replacing some repeated commands/variables with macros

--- trash_aux_files.sh
+++ trash_aux_files.sh
@@ -3,7 +3,7 @@
 # Remove all auxiliary files created by compiling thesis, including glossary and bibliography files
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-for file in ${this_dir}/labbook.*; do
+for file in ${this_dir}/labbook*; do
     if [[ $file == "${this_dir}/labbook.tex" ]] || [[ $file == "${this_dir}/labbook.pdf" ]]; then
 	    continue
     else
